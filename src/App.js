af19e7df9a0c6c500c42a62e0c4557ad87f1094f
bmbx08 <bmbx08@bu.ac.kr>
2024-07-23 02:32:36 +0900

    added reduction count

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import Box from './component/Box';
 
 function App() {
 
-  const[count,setCount]=useState(0);
+  let count = useSelector((state)=>state.count); //useSelector는 함수를 매개변수로 받음, 함수는 state를 매개변수로 받음.
+  let id = useSelector((state)=>state.id);
+  let password = useSelector((state)=>state.password);
+  let decCount=useSelector((state)=>state.decCount);
+  const dispatch = useDispatch();
 
   const increase=()=>{
-    setCount(count+1);
+    dispatch({type:"INCREMENT", payload: {num:5} }) //action은 객체다(type과 payload라는 키가 필요,type만 필수).
   }
 
-  useEffect(()=>{
+  const login = ()=>{
+    dispatch({type:"LOGIN",payload:{id:"Kevin",password:"123"}})
+  }
 
-  },[])
+  const decrease=(event)=>{
+    console.log(event.target.value);
+    if(event.key==="Enter"){
+      dispatch({type:"REDUCTION", payload:{decAmount:event.target.value}});
+    }
+    
+  }
 
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>Increase with redux</h1>
+      <h3>{count}</h3>
       <button onClick={increase}>Click me!</button>
+      <Box/>
+
+      
+
+      <h1>{id}{password}</h1>
+      <button onClick={login}>Login</button>
+
+      <h1>Decrease with redux</h1>
+      <h3>{decCount}</h3>
+      <input type="number" placeholder="Decrease by this much.." onKeyDown={(event)=>decrease(event)}/>
+
     </div>
   );
 }
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,10 +1,29 @@
 let initialState={
-    count:0
+    count:0,
+    id:"",
+    password:"",
+    decCount: 100,
 }
 
 
 function reducer(state=initialState,action){
+    console.log("action",action);
+    if(action.type==="INCREMENT"){  //reducer는 return문으로 state를 바꾸고 객체 형태로 store에 반환한다.
+        return{...state,count:state.count + action.payload.num};  //->다른 state값들은 유지하되, count의 state값만 바꾼다는 뜻 
+    } else if(action.type==="LOGIN"){
+        return{...state,id:action.payload.id, password:action.payload.password};
+    } else if(action.type==="REDUCTION"){
+        return{...state,decCount:state.decCount - action.payload.decAmount};
+    }
 
+    return {...state};
+
+    // switch(action.type){
+    //     case "INCREMENT":
+    //         return {...state,count:state.count+1};
+    //     default:
+    //         return {...state};
+    // } 
 }
 
 
